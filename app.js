7f4522f05ebd7a487b09f165ff00488c26d4e356
Cyril <ccyamoah@gmail.com>
2022-11-06 18:12:46 +0000

    homepage edit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const passport = require('passport');
 const { getMyBlogs } = require('./controllers/blogs');
@@ -15,7 +14,12 @@ const app = express();
 
 app.use(express.json());
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send(`<div>
+		<h1>Blog API</h1>
+		<p>use postman to test the various routes</p>
+		<p>See documentation <a target="_blank" rel="noopener noreferrer" href='https://github.com/ceesiyamoah/blogging_final_project#readme'>here</a></p>
+
+	</div>`);
 });
 app.use('/auth', authRouter);
 
